fe20a6c10b172c84cb4029ee3638bebb12b2b7aa
HeWeiChun <522187304@qq.com>
2020-12-14 00:01:15 +0800

    模板

--- pages/social/social.js
+++ pages/social/social.js
@@ -1,66 +1,46 @@
 // pages/social/social.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    currentTab: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  switchTab: function (e) {
+    let tab = e.currentTarget.id
+    if (tab === 'tableft') {
+      this.setData({
+        currentTab: 0
+      })
+    } else if (tab === 'tabmiddle') {
+      this.setData({
+        currentTab: 1
+      })
+    } else if (tab === 'tabright') {
+      this.setData({
+        currentTab: 2
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  switchSwiper: function (e) {
+    let tab = e.detail.currentItemId
+    if (tab === 'tableft') {
+      this.setData({
+        currentTab: 0
+      })
+    } else if (tab === 'tabmiddle') {
+      this.setData({
+        currentTab: 1
+      })
+    } else if (tab === 'tabright') {
+      this.setData({
+        currentTab: 2
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  onLoad: function (options) {},
+  onReady: function () {},
+  onShow: function () {},
+  onHide: function () {},
+  onUnload: function () {},
+  onPullDownRefresh: function () {},
+  onReachBottom: function () {},
+  onShareAppMessage: function () {}
 })
